ae69bce5b30ec2afdbf0ba3cb25feebf0b5e28db
Sai Gunturi <sai.gunturi@gmail.com>
2024-02-21 14:27:13 -0500

    Update bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,10 @@ DIR=$HOME/dotfiles
 DOTFILES=$DIR/dots
 
 for file in "$DOTFILES"/*; do
-  echo "Linking .$file..."
-  ln -s "$file" "$HOME/.$(basename $file)"
+  filename=$(basename "$file")        # Extract the basename of the file
+
+  rm -f "$HOME/.$filename"            # Remove the existing file or symlink in the home directory, if it exists
+
+  echo "Linking $HOME/.$filename..."
+  ln -s "$file" "$HOME/.$filename"    # Create a new symbolic link
 done
